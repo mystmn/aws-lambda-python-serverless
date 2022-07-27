9e40337fb2d2336daaf80ceb8dee43b3192ca01b
Paul Cameron <pcameron5@gmail.com>
2022-07-27 08:58:57 -0400

    WIP

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+db.init_app(app)
+# db.create_all()
+
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from .routes import main as main_blueprint
+app.register_blueprint(main_blueprint)
