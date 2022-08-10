c8ab1365df80845b0255d9c8a1650ea1a48d1e99
Paul Cameron <pcameron5@gmail.com>
2022-08-10 01:59:06 -0400

    CMOD-08102022: added Boto3, response from Dynamo

--- app/db/config.py
+++ app/db/config.py
@@ -1,8 +1,11 @@
-from ...app import app
+import app
 
-AWS_ACCESS_KEY_ID='AKIAWBZSFYUGW3ENUZNF'
-AWS_SECRET_ACCESS_KEY='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'
-REGION_NAME='us-east-1'
+# AWS_ACCESS_KEY_ID = 'AKIAWBZSFYUGW3ENUZNF'
+AWS_ACCESS_KEY_ID = 'AKIAWBZSFYUGXAYLN44C'
+# AWS_SECRET_ACCESS_KEY = '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'
+AWS_SECRET_ACCESS_KEY = 'Mve02M4XY4f/F1ynicv10QNHHoaTL1s543SnEu1N'
 
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'y
+AWS_REGION ='us-east-1'
+
+app.app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/db.sqlite'
